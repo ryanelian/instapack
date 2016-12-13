a3877f7c0fe70310528ff053ed79848c683f7475
Ryan Elian <ryan.elian.cs@gmail.com>
2016-12-14 05:29:42 +0700

    Removed tsc compiled stuffs.

--- ryan-compiler/client/js/aspnet-validation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// Hack: ASP.NET Core MVC JQuery Validation Unobtrusive requires that jQuery is defined globally!
-var $ = require("jquery");
-window['$'] = window['jQuery'] = $;
-// Hack: currently jquery-validation version higher than 1.15.0 breaks Browserify!
-require('jquery-validation');
-require('jquery-validation-unobtrusive');
-//# sourceMappingURL=aspnet-validation.js.map

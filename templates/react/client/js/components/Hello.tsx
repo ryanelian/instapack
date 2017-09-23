40e55dd27881ef508738073dfc1e3bf58fa99362
Ryan Elian <ryan.elian.cs@gmail.com>
2017-09-23 19:14:09 +0700

    Add react new templates!

---
+++ templates/react/client/js/components/Hello.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+interface HelloProps {
+    compiler: string;
+    framework: string;
+}
+
+// State is never set so we use the 'undefined' type.
+export class Hello extends React.Component<HelloProps, undefined> {
+    render() {
+        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+    }
+}
---
+++ templates/react/client/js/index.ts
@@ -0,0 +1,4 @@
+﻿import * as ES6Promise from 'es6-promise';
+import './react-project';
+
+ES6Promise.polyfill();

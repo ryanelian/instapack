87890db014dcd071d59335fd6d9b52608c3bc71f
Ryan Elian <ryan.elian.cs@gmail.com>
2017-09-23 18:09:39 +0700

    Add Vue.js new template!

---
+++ templates/vue/client/js/index.ts
@@ -0,0 +1,9 @@
+﻿import * as ES6Promise from 'es6-promise';
+import { ValidationService } from 'aspnet-validation';
+import 'bootstrap.native/dist/bootstrap-native-v4';
+import './vue-project';
+
+ES6Promise.polyfill();
+
+let v = new ValidationService();
+v.bootstrap();
---
+++ templates/vue/client/js/vue-project.ts
@@ -0,0 +1,11 @@
+import * as Vue from 'vue';
+
+import * as Components from './components';
+
+// components must be registered BEFORE the app root declaration
+Vue.component('hello', Components.HelloWorld);
+
+// bootstrap the Vue app from the root element <div id="app"></div>
+new Vue({
+    el: '#app'
+});

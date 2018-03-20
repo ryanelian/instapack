01b0a4477555e1a405728f793a2a98b7bf404e7e
Ryan Elian <ryan.elian.cs@gmail.com>
2018-03-21 04:33:00 +0700

    Update inferno packages to version 5. Remove re-exports!

--- templates/inferno/client/js/inferno-project.tsx
+++ templates/inferno/client/js/inferno-project.tsx
@@ -1,6 +1,6 @@
 import { render } from 'inferno';
 import { createElement } from 'inferno-create-element';
-import { Hello } from "./components";
+import { Hello } from "./components/Hello";
 
 render(
     <Hello compiler="instapack" framework="Inferno" />,

c9a45f545a486ebeda3764ac4376fdb6480afae8
Ryan Elian <ryan.elian.cs@gmail.com>
2018-04-16 17:45:40 +0700

    Convert vue-mobile template to Single-File Component.

--- templates/vue-mobile/client/js/components/NotHome.ts
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import Component from 'vue-class-component';
-import { render, staticRenderFns } from './NotHome.vue.html';
-
-@Component({
-    render, staticRenderFns
-})
-export class NotHome extends Vue {
-}
--- templates/vue-mobile/client/js/components/Root.vue
+++ templates/vue-mobile/client/js/components/Root.vue
@@ -1,18 +1,22 @@
+<template>
+<v-ons-navigator swipeable :page-stack="pageStack" @push-page="pageStack.push($event)"></v-ons-navigator>
+</template>
+
+<script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
-import { render, staticRenderFns } from './Root.vue.html';
-import { Home } from './Home';
+import Home from './Home.vue';
 
 @Component({
-    render, staticRenderFns,
     created: function (this: Root) {
         this.initialize();
     }
 })
-export class Root extends Vue {
+export default class Root extends Vue {
     pageStack = [];
 
     initialize() {
         this.pageStack.push(Home);
     }
 }
+</script>

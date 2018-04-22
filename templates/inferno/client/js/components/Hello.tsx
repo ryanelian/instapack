fdb78d88ab7efe2faedb0179ec88f845f943d97d
Ryan Elian <ryan.elian.cs@gmail.com>
2018-04-23 02:52:16 +0700

    Use babel-plugin-inferno to compile JSX!

--- templates/inferno/client/js/components/Hello.tsx
+++ templates/inferno/client/js/components/Hello.tsx
@@ -1,12 +1,10 @@
 import { Component } from 'inferno';
-import { createElement } from 'inferno-create-element';
 
 interface HelloProps {
     compiler: string;
     framework: string;
 }
 
-// State is never set so we use the `undefined` type.
 export class Hello extends Component<HelloProps, undefined> {
     render() {
         return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;

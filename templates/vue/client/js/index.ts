9485d00e2e373537299a86ea8d0031574450af79
Ryan Elian <ryan.elian.cs@gmail.com>
2018-04-24 04:50:21 +0700

    Replace es6-promise with ts-polyfill in Vue new project template.

--- templates/vue/client/js/index.ts
+++ templates/vue/client/js/index.ts
@@ -1,4 +1,10 @@
-﻿import { ValidationService } from 'aspnet-validation';
+﻿import 'ts-polyfill/lib/es2015-core';
+import 'ts-polyfill/lib/es2015-promise';
+import 'ts-polyfill/lib/es2016-array-include';
+import 'ts-polyfill/lib/es2017-string';
+import 'ts-polyfill/lib/es2018-promise';
+
+import { ValidationService } from 'aspnet-validation';
 import './icons';
 import './vue-project';
 

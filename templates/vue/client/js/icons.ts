b5a871d5e4b189ac2174674cae1fc1bce24250c4
Ryan Elian <ryan.elian.cs@gmail.com>
2018-04-30 23:25:55 +0700

    Improve vue template.

--- templates/vue/client/js/icons.ts
+++ templates/vue/client/js/icons.ts
@@ -1,9 +1,8 @@
-import fontawesome from '@fortawesome/fontawesome';
-import fas from '@fortawesome/fontawesome-free-solid';
-import far from '@fortawesome/fontawesome-free-regular';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { } from '@fortawesome/free-regular-svg-icons';
 
-fontawesome.library.add(fas);
-fontawesome.library.add(far);
+library.add(faChevronUp);
 
 // Reduce dll size by only importing icons which are actually being used:
 // https://fontawesome.com/how-to-use/use-with-node-js
--- templates/vue/client/js/index.ts
+++ templates/vue/client/js/index.ts
@@ -1,10 +1,4 @@
-﻿import 'ts-polyfill/lib/es2015-core';
-import 'ts-polyfill/lib/es2015-promise';
-import 'ts-polyfill/lib/es2016-array-include';
-import 'ts-polyfill/lib/es2017-string';
-import 'ts-polyfill/lib/es2018-promise';
-
-import { ValidationService } from 'aspnet-validation';
+﻿import { ValidationService } from 'aspnet-validation';
 import './icons';
 import './vue-project';
 

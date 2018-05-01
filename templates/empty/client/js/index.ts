32b584355220e83ad99f8f85475e29d29463ba87
Ryan Elian <ryan.elian.cs@gmail.com>
2018-05-01 13:08:03 +0700

    Simplify ts-polyfill import. Faster build due to pre-bundled library.

--- templates/empty/client/js/index.ts
+++ templates/empty/client/js/index.ts
@@ -1,5 +1,1 @@
-﻿import 'ts-polyfill/lib/es2015-core';
-import 'ts-polyfill/lib/es2015-promise';
-import 'ts-polyfill/lib/es2016-array-include';
-import 'ts-polyfill/lib/es2017-string';
-import 'ts-polyfill/lib/es2018-promise';
+﻿import 'ts-polyfill';
--- templates/mithril/client/js/index.ts
+++ templates/mithril/client/js/index.ts
@@ -1,7 +1,3 @@
-﻿import 'ts-polyfill/lib/es2015-core';
-import 'ts-polyfill/lib/es2015-promise';
-import 'ts-polyfill/lib/es2016-array-include';
-import 'ts-polyfill/lib/es2017-string';
-import 'ts-polyfill/lib/es2018-promise';
+﻿import 'ts-polyfill';
 
 import './mithril-project';

cb1721b5e874ad29cb598e7a76e006381cff7a41
Ryan Elian <ryan.elian.cs@gmail.com>
2018-05-01 13:13:37 +0700

    Streamline template: vue-mobile should use ts-polyfill.

--- templates/vue-mobile/client/js/index.ts
+++ templates/vue-mobile/client/js/index.ts
@@ -1,4 +1,6 @@
-﻿import ons from 'onsenui';
+﻿import 'ts-polyfill';
+import ons from 'onsenui';
+
 if (ons.platform.isIPhoneX()) {
     // https://onsen.io/v2/guide/iphonex.html#iphone-x
     document.documentElement.setAttribute('onsflag-iphonex-portrait', '');

7224d60d6e0c80956beb4d399129571918680cd9
Ryan Elian <ryan.elian.cs@gmail.com>
2018-09-11 22:44:55 +0700

    Add unit tests for compile variables method.

--- bin/VariablesFactory.test.js
+++ bin/VariablesFactory.test.js
@@ -156,3 +156,114 @@ ava_1.default('Parse CLI env: Invalid Array', t => {
     let r = v.parseCliEnv(['a', 'b', 'c']);
     t.deepEqual(r, {});
 });
+ava_1.default('Compile Variables: Simple', t => {
+    let result = v.compile({
+        env: {
+            foo: 'bar'
+        },
+        hot: false,
+        production: true,
+        watch: false,
+        sourceMap: true,
+        stats: true,
+        verbose: false
+    }, {
+        root: __dirname,
+        input: 'client',
+        output: 'wwwroot',
+        jsOut: 'ipack.js',
+        cssOut: 'ipack.css',
+        alias: {},
+        externals: {},
+        port1: 0,
+        port2: 0
+    }, {
+        muteNotification: false,
+        packageManager: 'yarn'
+    }, {
+        hello: 'world'
+    });
+    let expected = {
+        root: __dirname,
+        input: 'client',
+        output: 'wwwroot',
+        jsOut: 'ipack.js',
+        cssOut: 'ipack.css',
+        alias: {},
+        externals: {},
+        port1: 0,
+        port2: 0,
+        muteNotification: false,
+        packageManager: 'yarn',
+        env: {
+            foo: 'bar',
+            hello: 'world'
+        },
+        hot: false,
+        production: true,
+        watch: false,
+        sourceMap: true,
+        stats: true,
+        verbose: false
+    };
+    t.deepEqual(result, expected);
+});
+ava_1.default('Compile Variables: Overrides', t => {
+    let result = v.compile({
+        env: {
+            foo: 'bar',
+        },
+        hot: true,
+        production: true,
+        watch: false,
+        sourceMap: false,
+        stats: true,
+        verbose: true
+    }, {
+        root: __dirname,
+        input: 'src',
+        output: 'www',
+        jsOut: 'bundle.js',
+        cssOut: 'bundle.css',
+        alias: {
+            'vue': 'vue/dist/vue.esm'
+        },
+        externals: {
+            jquery: '$'
+        },
+        port1: 20178,
+        port2: 20179
+    }, {
+        muteNotification: true,
+        packageManager: 'npm'
+    }, {
+        foo: 'zero'
+    });
+    let expected = {
+        root: __dirname,
+        input: 'src',
+        output: 'www',
+        jsOut: 'bundle.js',
+        cssOut: 'bundle.css',
+        alias: {
+            'vue': 'vue/dist/vue.esm'
+        },
+        externals: {
+            jquery: '$'
+        },
+        port1: 20178,
+        port2: 20179,
+        muteNotification: true,
+        packageManager: 'npm',
+        env: {
+            foo: 'bar'
+        },
+        hot: true,
+        production: false,
+        watch: true,
+        sourceMap: false,
+        stats: false,
+        verbose: true
+    };
+    t.deepEqual(result, expected);
+});

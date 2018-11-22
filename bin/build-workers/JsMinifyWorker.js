4fa5f73489e6408de8d2213a5d1693b1cbd47846
Ryan Elian <ryan.elian.cs@gmail.com>
2018-11-23 00:48:09 +0700

    Backport UglifyJS fix from 7.0.0. Pin to last good version.

--- bin/build-workers/JsMinifyWorker.js
+++ bin/build-workers/JsMinifyWorker.js
@@ -1,12 +1,15 @@
 "use strict";
 const UglifyJS = require("uglify-js");
 module.exports = function (input, callback) {
-    let minifyOptions;
+    let minifyOptions = {
+        compress: {
+            collapse_vars: false,
+            conditionals: false,
+        }
+    };
     if (input.map) {
-        minifyOptions = {
-            sourceMap: {
-                content: input.map
-            }
+        minifyOptions.sourceMap = {
+            content: input.map
         };
     }
     let result = UglifyJS.minify({
--- src/build-workers/JsMinifyWorker.ts
+++ src/build-workers/JsMinifyWorker.ts
@@ -5,13 +5,20 @@ import { IMinifyInputs } from '../IMinifyInputs';
  * Accepts minification input parameter to used by UglifyJS minifier.
  */
 export = function (input: IMinifyInputs, callback) {
-    let minifyOptions: UglifyJS.MinifyOptions;
-    if (input.map) {
-        minifyOptions = {
-            sourceMap: {
-                content: input.map as any // HACK78
-            }
+    // collapse_vars issues:
+    // 1. performance regressions   https://github.com/mishoo/UglifyJS2/issues/3174
+    // 2. bad output                https://github.com/mishoo/UglifyJS2/issues/3247
+    let minifyOptions: UglifyJS.MinifyOptions = {
+        compress: {
+            collapse_vars: false,
+            conditionals: false,     // https://github.com/mishoo/UglifyJS2/issues/3245#issuecomment-417940973
         }
+    };
+
+    if (input.map) {
+        minifyOptions.sourceMap = {
+            content: input.map as any // HACK78
+        };
     }
 
     let result = UglifyJS.minify({

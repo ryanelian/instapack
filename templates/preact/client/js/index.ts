101828cfe2c39af71c333a2a4268691dd7db08bd
Ryan Elian <ryan.elian.cs@gmail.com>
2019-05-17 03:39:15 +0700

    Update templates to use ES2015 tsconfig!

--- templates/preact/client/js/index.ts
+++ templates/preact/client/js/index.ts
@@ -1,2 +1,10 @@
-import 'ts-polyfill';
+import 'ts-polyfill/lib/es2016-array-include';
+import 'ts-polyfill/lib/es2017-object';
+import 'ts-polyfill/lib/es2017-string';
+import 'ts-polyfill/lib/es2017-async-iterable';   // for-await-of
+import 'ts-polyfill/lib/es2018-promise';
+import 'ts-polyfill/lib/es2019-array';
+import 'ts-polyfill/lib/es2019-string';
+import 'ts-polyfill/lib/es2019-symbol';
+
 import './preact-project';

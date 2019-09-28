0cfedd8a96fa36c1e77185817cefbfe8180edaa9
Ryan Elian <ryan.elian.cs@gmail.com>
2019-09-28 19:38:13 +0700

    Remove stale vue-mobile template. FROM THE ASHES, THE PHOENIX REBORNS. VIVA LA BLAZOR NATIVE.

--- templates/vue-mobile/client/js/vue-project.ts
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import VueOnsen from 'vue-onsenui';
-import Root from './components/Root.vue';
-
-Vue.use(VueOnsen);
-
-// bootstrap the Vue app from the root element <div id="app"></div>
-new Root().$mount('#app');

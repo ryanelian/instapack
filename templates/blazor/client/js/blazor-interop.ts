ad8f9d6a1cc9e5a04693c12d6c93826d93922c8c
Ryan Elian <ryan.elian.cs@gmail.com>
2019-12-30 19:11:12 +0700

    Add comments about Blazor JS interop.

--- templates/blazor/client/js/blazor-interop.ts
+++ templates/blazor/client/js/blazor-interop.ts
@@ -1,5 +1,8 @@
 import swal from 'sweetalert';
 
+// this function can be invoked in Blazor using IJSRuntime:
+// await JsRuntime.InvokeVoidAsync("instapack.hello");
+// https://docs.microsoft.com/en-us/aspnet/core/blazor/javascript-interop?view=aspnetcore-3.1
 export async function hello(): Promise<void> {
     await swal({
         title: 'Hello World!',
--- templates/blazor/client/js/index.ts
+++ templates/blazor/client/js/index.ts
@@ -10,4 +10,7 @@ import 'ts-polyfill/lib/es2020-string';
 
 import 'bootstrap';
 
+// re-export all modules to global namespace object
+// (window[namespace]) by default: window['instapack']
+// namespace name can be set at package.json:instapack:namespace
 export * from './blazor-interop';

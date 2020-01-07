4ab4b93279200a2edada459de9a48964333220bf
Ryan Elian <ryan.elian.cs@gmail.com>
2020-01-07 18:48:11 +0700

    Greatly simplify templates. Update remaining.

--- templates/inferno/client/js/inferno-project.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from 'inferno';
-import { Hello } from "./components/Hello";
-
-render(
-    <Hello compiler="instapack" framework="Inferno" />,
-    document.getElementById('app')
-);
--- templates/react/client/js/components/Hello.tsx
+++ templates/react/client/js/components/Hello.tsx
@@ -2,15 +2,25 @@ import React from 'react';
 import { Alert } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
 
 interface HelloProps {
     compiler: string;
     framework: string;
 }
 
-export class Hello extends React.Component<HelloProps, {}> {
+@observer
+export class Hello extends React.Component<HelloProps> {
+    @observable isVisible = true;
+
+    // This syntax ensures `this` is bound within onDismiss.
+    onDismiss = (): void => {
+        this.isVisible = false;
+    }
+
     render(): JSX.Element {
-        return <Alert color="success">
+        return <Alert color="success" toggle={this.onDismiss} isOpen={this.isVisible}>
             <FontAwesomeIcon className="mr-3" icon={faCheckCircle}></FontAwesomeIcon>
             Hello from {this.props.compiler} and {this.props.framework}!
         </Alert>

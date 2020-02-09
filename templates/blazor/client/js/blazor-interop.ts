f3e921a6222370cb6bc942730e0bd971434ad530
Ryan Elian <ryan.elian.cs@gmail.com>
2020-02-10 05:31:42 +0700

    Update template packages...

--- templates/blazor/client/js/blazor-interop.ts
+++ templates/blazor/client/js/blazor-interop.ts
@@ -1,12 +1,6 @@
-import swal from 'sweetalert';
-
 // this function can be invoked in Blazor using IJSRuntime:
-// await JsRuntime.InvokeVoidAsync("instapack.hello");
+// await JsRuntime.InvokeVoidAsync("instapack.hello", "Accelist");
 // https://docs.microsoft.com/en-us/aspnet/core/blazor/javascript-interop?view=aspnetcore-3.1
-export async function hello(): Promise<void> {
-    await swal({
-        title: 'Hello World!',
-        text: 'This message is brought to you by instapack + Blazor',
-        icon: "success",
-    });
+export async function hello(name: string): Promise<void> {
+    console.log(`Hello, ${name}!`);
 }
